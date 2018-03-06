6726089da8e965595e1bb6dd2b069ca7b0a6cd30
877942720@qq.com <lsj890305>
2018-03-06 14:39:28 +0800

    7-4

--- application/api/model/Banner.php
+++ application/api/model/Banner.php
@@ -15,7 +15,9 @@ class Banner
 {
     public static function getBannerById($id)
     {
-        $result = Db::query('select * from banner_item WHERE banner_id=?',[$id]);
+//        $result = Db::query('select * from banner_item WHERE banner_id=?',[$id]);
+//        return $result;
+        $result = Db::table('banner_item') -> where('banner_id','=',$id) -> select();
         return $result;
     }
 }
