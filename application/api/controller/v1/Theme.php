b5e0bd215dd81046e50c7fe77b01e9e497c75f7b
877942720@qq.com <lsj890305>
2018-03-07 14:03:37 +0800

    8-10

---
+++ application/api/controller/v1/Theme.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 15618
+ * Date: 2018/3/7
+ * Time: 14:01
+ */
+
+namespace app\api\controller\v1;
+
+
+class Theme
+{
+
+}
---
+++ application/api/model/Theme.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 15618
+ * Date: 2018/3/7
+ * Time: 14:02
+ */
+
+namespace app\api\model;
+
+
+class Theme extends BaseModel
+{
+
+}
