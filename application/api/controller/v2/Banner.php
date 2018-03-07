63dd8433722a21444913d3fac76cc58419db63c4
877942720@qq.com <lsj890305>
2018-03-07 13:47:50 +0800

    8-9

---
+++ application/api/controller/v2/Banner.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/3/003
+ * Time: 15:32
+ */
+
+namespace app\api\controller\v2;
+
+
+class Banner
+{
+    public function getBanner($id){
+       return 'This is v2 banner';
+    }
+}
