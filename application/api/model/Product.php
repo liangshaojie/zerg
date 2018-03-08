d772bd751905bb02243c4a6a10d29b3795b803e1
877942720@qq.com <lsj890305>
2018-03-08 14:41:26 +0800

    8-22

--- application/api/model/Product.php
+++ application/api/model/Product.php
@@ -23,4 +23,9 @@ class Product extends BaseModel
         $products = self::limit($count) -> order('create_time desc') -> select();
         return $products;
     }
+
+    public static function getProductsByCategoryID($categoryID){
+        $products = self::where('category_id','=',$categoryID) ->select();
+        return $products;
+    }
 }
