6a8cb067fd13fc53425cfb16f1ae33c9ac32a7a2
liangshaojie <877942720@qq.com>
2018-03-10 12:09:26 +0800

    9-13

--- application/api/controller/v1/Address.php
+++ application/api/controller/v1/Address.php
@@ -9,9 +9,14 @@
 namespace app\api\controller\v1;
 
 
+use app\api\validate\AddressNew;
+use app\api\service\Token as TokenService;
+
 class Address
 {
     public function createOrUpdateAddress(){
+        (new AddressNew()) -> goCheck();
 
+        $uid = TokenService::getCurrentUid();
     }
 }
--- application/api/service/Token.php
+++ application/api/service/Token.php
@@ -9,6 +9,11 @@
 namespace app\api\service;
 
 
+use app\lib\exception\TokenException;
+use think\Cache;
+use think\Exception;
+use think\Request;
+
 class Token
 {
     // 生成令牌
@@ -19,4 +24,30 @@ class Token
         $tokenSalt = config('secure.token_salt');
         return md5($randChar . $timestamp . $tokenSalt);
     }
+
+    public static function getCurrentTokenVar($key){
+        $token = Request::instance()
+            -> header('token');
+        $vars =Cache::get($token);
+        if(!$vars){
+            throw new TokenException();
+        }else{
+            if(!is_array($vars)){
+                $vars = json_decode($vars,true);
+            }
+            if(array_key_exists($key,$vars)){
+                return $vars[$key];
+            }else{
+                throw new Exception('尝试获取的token变量并不存在！');
+            }
+        }
+    }
+
+    public static function getCurrentUid(){
+        $uid = self::getCurrentTokenVar('uid');
+        return $uid;
+    }
+
+
+
 }
