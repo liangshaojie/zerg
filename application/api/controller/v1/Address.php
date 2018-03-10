621ba81e3dacb7944fc88554ad401cd8563e8557
liangshaojie <877942720@qq.com>
2018-03-10 11:52:11 +0800

    9-12

---
+++ application/api/controller/v1/Address.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/10/010
+ * Time: 11:39
+ */
+
+namespace app\api\controller\v1;
+
+
+class Address
+{
+    public function createOrUpdateAddress(){
+
+    }
+}
---
+++ application/api/validate/AddressNew.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/10/010
+ * Time: 11:42
+ */
+
+namespace app\api\validate;
+
+
+class AddressNew extends BaseValidate
+{
+    protected $rule = [
+        'name' => 'require|isNotEmpty',
+        'mobile' => 'require|isMobile',
+        'province' => 'require|isNotEmpty',
+        'city' => 'require|isNotEmpty',
+        'country' => 'require|isNotEmpty',
+        'detail' => 'require|isNotEmpty',
+    ];
+}
